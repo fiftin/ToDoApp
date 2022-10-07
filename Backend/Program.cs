763ae773c53f435bd99beca4e4e8cdfe349bfa18
Denis Gukov <fiftin@outlook.com>
2022-10-07 13:19:42 +0500

    feat(backend): use windows service

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,4 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.Hosting.WindowsServices;
+
+var options = new WebApplicationOptions
+{
+    Args = args,
+    ContentRootPath = WindowsServiceHelpers.IsWindowsService()
+                                     ? AppContext.BaseDirectory : default
+};
+
+var builder = WebApplication.CreateBuilder(options);
 
 // Add services to the container.
 
@@ -6,6 +15,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.UseWindowsService();
 
 var app = builder.Build();
 
